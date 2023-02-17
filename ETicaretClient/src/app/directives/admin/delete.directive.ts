948822027fa00fc20035052f2752676dc64bce41
Ebru Ateş <devatesebru@gmail.com>
2023-02-17 13:19:16 +0300

    Delete Modal Dialog İle Silme Onaylama

--- ETicaretClient/src/app/directives/admin/delete.directive.ts
+++ ETicaretClient/src/app/directives/admin/delete.directive.ts
@@ -3,7 +3,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from '../../base/base.component';
 import { HttpClientService } from '../../services/common/http-client.service';
 import { ProductService } from '../../services/common/models/product.service';
-
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
 declare var $: any;
 
 @Directive({
@@ -17,7 +18,8 @@ export class DeleteDirective {
     private element: ElementRef,
     private _renderer: Renderer2,
     private productService: ProductService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete.png");
@@ -32,11 +34,30 @@ export class DeleteDirective {
 
   @HostListener("click")
   async onclick() {
-    this.spinner.show(SpinnerType.BallAtom);
-    const td: HTMLTableCellElement = this.element.nativeElement;
-    await this.productService.delete(this.id);
-    $(td.parentElement).fadeOut(2000, () => {this.callback.emit() });
 
+    this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallAtom);
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      await this.productService.delete(this.id);
+      $(td.parentElement).fadeOut(2000, () => { this.callback.emit() });
+
+    });   
+
+  }
+
+
+
+  openDialog(afterClosed:any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width:'250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == DeleteState.Yes) 
+        afterClosed();
+      
+    });
   }
 
 }
