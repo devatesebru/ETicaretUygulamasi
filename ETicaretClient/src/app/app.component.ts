80787feddc83e1c71a155550474980303e919917
Ebru Ateş <devatesebru@gmail.com>
2023-05-07 22:27:32 +0300

    IdentityCheck İle Link Kontrolü-ReturnURL İşlevi

--- ETicaretClient/src/app/app.component.ts
+++ ETicaretClient/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { MessageType, Position } from './services/admin/alertify.service';
+import { AuthService } from './services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
@@ -11,8 +13,18 @@ declare var $: any
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title='ETicaretClient';
-  constructor() {
+  constructor(public authService: AuthService, private toastrService: CustomToastrService, private router: Router) {
+
+    authService.identityCheck();
+  }
+  signOut() {
+    localStorage.removeItem("accessToken");
+    this.authService.identityCheck();
+    this.router.navigate([""]);
+    this.toastrService.message("Oturum kapatılmıştır!", "Oturum Kapatıldı", {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.TopRight
+    })
 
   }
 
--- ETicaretClient/src/app/guards/common/auth.guard.ts
+++ ETicaretClient/src/app/guards/common/auth.guard.ts
@@ -4,6 +4,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable } from 'rxjs';
 import { SpinnerType } from '../../base/base.component';
+import { AuthService, _isAuthenticated } from '../../services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../services/ui/custom-toastr.service';
 
 @Injectable({
@@ -19,17 +20,17 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     this.spinner.show(SpinnerType.BallAtom);
 
-    const token: string = localStorage.getItem("accessToken");
+   // const token: string = localStorage.getItem("accessToken");
 
-   // const decodetoken = this.jwtHelper.decodeToken(token);
-   // const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
-    let expired: boolean;
-    try {
-      expired = this.jwtHelper.isTokenExpired(token);
-    } catch (e) {
-      expired = true;
-    }
-    if (!token || expired) {
+   //// const decodetoken = this.jwtHelper.decodeToken(token);
+   //// const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
+   // let expired: boolean;
+   // try {
+   //   expired = this.jwtHelper.isTokenExpired(token);
+   // } catch (e) {
+   //   expired = true;
+    // }
+    if (!_isAuthenticated) {
       this.router.navigate(["login"], { queryParams: { returnUrl: state.url } });
       this.toastrService.message("Oturum açın lütfen ", "Yetkisiz Erişim!", {
         messageType: ToastrMessageType.Warning,
