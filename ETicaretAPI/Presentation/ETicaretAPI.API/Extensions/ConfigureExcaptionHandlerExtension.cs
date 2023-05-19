833e7030ee9eb8bb6870f0d34b53d5c5ad62cda0
Ebru Ateş <devatesebru@gmail.com>
2023-05-19 15:15:17 +0300

    Backend'de Global Exception Handler Oluşturma

---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Extensions/ConfigureExcaptionHandlerExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+
+namespace ETicaretAPI.API.Extensions
+{
+    static public class ConfigureExcaptionHandlerExtension
+    {
+        public static void ConfigureExceptionHandler<T>(this WebApplication application, ILogger<T> logger)
+        {
+            application.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                  var contextFeature=  context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature != null)
+                    {
+                        logger.LogError(contextFeature.Error.Message);
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                        {
+                           StatusCode = context.Response.StatusCode,
+                           Message = contextFeature.Error.Message,
+                           Title = "hata alındı!"
+
+                        })); 
+                    }
+                });
+            });
+        }
+    }
+}
--- ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.API.Configurations.ColumnWriters;
+using ETicaretAPI.API.Extensions;
 using ETicaretAPI.Application;
 using ETicaretAPI.Application.Validators.Products;
 using ETicaretAPI.Infrastructure;
@@ -94,6 +95,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureExceptionHandler<Program>(app.Services.GetRequiredService<ILogger<Program>>());
+
 
 app.UseStaticFiles();
 
